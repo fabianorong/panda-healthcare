b7721e75d67f4b3375d474ea24cb90e1cba0284a
Fabiano Rong <fabianorong@gmail.com>
2024-10-08 12:52:26 -0300

    sentry removed

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -11,7 +11,7 @@ import {
   databases,
   messaging,
 } from "../appwrite.config";
-import { formatDateTime, parseStringify } from "../utils";
+import { parseStringify } from "../utils";
 
 //  CREATE APPOINTMENT
 export const createAppointment = async (
@@ -77,27 +77,10 @@ export const getRecentAppointmentList = async () => {
   }
 };
 
-//  SEND SMS NOTIFICATION
-export const sendSMSNotification = async (userId: string, content: string) => {
-  try {
-    // https://appwrite.io/docs/references/1.5.x/server-nodejs/messaging#createSms
-    const message = await messaging.createSms(
-      ID.unique(),
-      content,
-      [],
-      [userId]
-    );
-    return parseStringify(message);
-  } catch (error) {
-    console.error("An error occurred while sending sms:", error);
-  }
-};
-
 //  UPDATE APPOINTMENT
 export const updateAppointment = async ({
   appointmentId,
   userId,
-  // timeZone,
   appointment,
   type,
 }: UpdateAppointmentParams) => {
@@ -111,17 +94,6 @@ export const updateAppointment = async ({
     );
 
     if (!updatedAppointment) throw Error;
-
-    // const smsMessage = `Greetings from CarePulse. ${
-    //   type === "schedule"
-    //     ? `Your appointment is confirmed for ${
-    //         formatDateTime(appointment.schedule!, timeZone).dateTime
-    //       } with Dr. ${appointment.primaryPhysician}`
-    //     : `We regret to inform that your appointment for ${
-    //         formatDateTime(appointment.schedule!, timeZone).dateTime
-    //       } is cancelled. Reason:  ${appointment.cancellationReason}`
-    // }.`;
-    // await sendSMSNotification(userId, smsMessage);
 
     revalidatePath("/admin");
     return parseStringify(updatedAppointment);
